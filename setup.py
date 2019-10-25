a1652aa22be1dd16bf88f7a8e8e2532b8d747746
Andreas Hasenkopf <ahasenkopf@suse.de>
2019-10-25 16:43:56 +0200

    Re-release as `django-auditlog2`

--- setup.py
+++ setup.py
@@ -1,26 +1,41 @@
-from distutils.core import setup
+from setuptools import setup, find_packages, find_namespace_packages
+
+try:
+    with open("README.md") as fh:
+        long_description = fh.read()
+except:
+    long_description = ""
 
 setup(
-    name='django-auditlog',
-    version='0.4.5',
-    packages=['auditlog', 'auditlog.migrations', 'auditlog.management', 'auditlog.management.commands'],
-    package_dir={'': 'src'},
-    url='https://github.com/jjkester/django-auditlog',
+    name='django-auditlog2',
+    version='0.5.0',
+    packages=find_packages(where="src", exclude=['*tests*', 'src']),
+    package_dir={"": "src"},
+    url='https://github.com/Canned-Django/django-auditlog',
     license='MIT',
-    author='Jan-Jelle Kester',
+    author='Andreas Hasenkopf',
+    author_email='andi@hasenkopf2000.net',
     description='Audit log app for Django',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=[
-        'django-jsonfield>=1.0.0',
-        'python-dateutil==2.6.0'
+        'django-jsonfield',
+        'python-dateutil'
     ],
     zip_safe=False,
     classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'License :: OSI Approved :: MIT License',
     ],        
 )
