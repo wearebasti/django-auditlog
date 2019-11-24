7a34385ebadd4846685ceca82543f0032b228392
basti <12233784+sebastianseitz@users.noreply.github.com>
2019-11-24 21:40:12 +0100

    make test-progress more visible; set up envs

--- src/runtests.py
+++ src/runtests.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     os.environ['DJANGO_SETTINGS_MODULE'] = 'auditlog_tests.test_settings'
     django.setup()
     TestRunner = get_runner(settings)
-    test_runner = TestRunner()
+    test_runner = TestRunner(verbosity=2)
     failures = test_runner.run_tests(["auditlog_tests"])
     sys.exit(bool(failures))
